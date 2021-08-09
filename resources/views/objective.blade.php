413f262b6c0408f9c05267c7448db270630982de
Anthony Atkinson <anthonyjamesatkinson@gmail.com>
2021-08-09 13:08:11 -0400

    Separating sections into files

---
+++ resources/views/objective.blade.php
@@ -0,0 +1,13 @@
+<div id="objective">
+    <h1>Objective</h1>
+
+    <section>
+        <p>
+            To utilize the maximum portion of my skill set possible,
+            while providing opportunities to grow the productivity and
+            effectiveness of the business, using technology to create
+            results and turn complex business logic into simple,
+            meaningful software.
+        </p>
+    </section>
+</div>
