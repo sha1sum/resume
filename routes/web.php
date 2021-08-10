2db11207e10f3cd08b5d990e1dd4d2743442477a
Anthony Atkinson <anthonyjamesatkinson@gmail.com>
2021-08-10 19:04:06 -0400

    Employer integration

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Employer;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('resume');
+    return view('resume', [
+        'employers' => Employer::orderBy('sort_order', 'ASC')->get()
+    ]);
 });
